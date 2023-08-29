1592fb5fd85427f378f2f753c10db2827564230d
Abayomi Bello-Data <94766302+AbayomiBello-Data@users.noreply.github.com>
2023-08-30 00:10:48 +0100

    Update fruit.py

--- fruit.py
+++ fruit.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 # Constants
 IMAGE_SIZE = 160
-MODEL_PATH = "MobileNetV2.h5"
+MODEL_PATH = "MobileNetV2 (1).h5"
 
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -15,24 +15,28 @@ def load_model():
 
 def preprocess_image(image: Image.Image) -> np.ndarray:
     """Preprocess the input image to make it ready for prediction."""
-    # Resize and scale
     image = image.resize((IMAGE_SIZE, IMAGE_SIZE))
     image = np.array(image) / 255.0
     image = image.astype(np.float32)
-    # Add batch dimension
     image = np.expand_dims(image, axis=0)
     return image
 
-# Load the model
-model = load_model()
+@st.cache
+def load_class_names():
+    _, unique_idx = np.unique(y_train, return_index=True)
+    unique_fruit_names = [class_names_train[label] for label in y_train[unique_idx]]
+    return sorted(unique_fruit_names)
 
-# Set the title and the layout of the Streamlit app
+# Load the model and class names
+model = load_model()
+class_names = load_class_names()
+
+# Streamlit UI code
 st.title("Fruit Classifier")
 st.write("""
 Upload an image of a fruit and the model will predict its name.
 """)
 
-# Upload the image
 uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
@@ -40,12 +44,9 @@ if uploaded_image is not None:
     st.image(image, caption="Uploaded Image.", use_column_width=True)
     st.write("")
     st.write("Classifying...")
-
-    # Preprocess and predict
+    
     processed_image = preprocess_image(image)
     predictions = model.predict(processed_image)
     predicted_class = np.argmax(predictions)
-
-    # Display the prediction
-    class_names = ['Apple', 'Banana', 'Grape', 'Mango', 'Strawberry']  # Adjust based on your classes
+    
     st.write(f"The model predicts this fruit as: {class_names[predicted_class]}")
